b26c4bd64bc5bcf556cb5e62a38496915b70b558
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-19 20:52:00 +0700

    update header of the prompt and navbar placement

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { logo } from "../assets/";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
         <button
           type="button"
@@ -12,6 +12,10 @@ const Hero = () => {
           Github
         </button>
       </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
     </header>
   );
 };
