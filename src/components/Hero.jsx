d23075a8c5f9ac7598e2427ae037d4e18b2ff33c
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-19 20:54:55 +0700

    complete the navbar header and description of the open source projectr

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,9 +13,13 @@ const Hero = () => {
         </button>
       </nav>
       <h1 className="head_text">
-        Summarize Articles with <br />
+        Summarize Articles with <br className="max-md:hidden" />
         <span className="orange_gradient">OpenAI GPT-4</span>
       </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open source article summarizer
+        that transform lengthy articles into clear and concise summaries
+      </h2>
     </header>
   );
 };
