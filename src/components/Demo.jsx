1a1c1718b1b25df8c1071a08b11d858e72e03037
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-20 23:21:16 +0700

    complete the demo component project

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -21,7 +21,6 @@ const Demo = () => {
     const { data } = await getSummary({ articleUrl: article.url });
 
     if (data?.summary) {
-      console.log(data);
       const newArticle = { ...article, summary: data.summary };
       const updatedAllArticles = [newArticle, ...allArticles];
       setArticle(newArticle);
@@ -83,7 +82,34 @@ const Demo = () => {
           ))}
         </div>
       </div>
+
       {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            That wasn't supposed to happen
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
