87c350bba445026f98a07e768b6e41fed6d31254
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-20 23:05:22 +0700

    update value of host

--- src/services/article.js
+++ src/services/article.js
@@ -1,15 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
-const rapidApiHost = import.meta.env.VITE_RAPID_API_ARTICLE_HOST;
 
 export const articleApi = createApi({
 	reducerPath: "articleApi",
 	baseQuery: fetchBaseQuery({
-		baseUrl:
-			"https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
+		baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com",
 		prepareHeaders: (headers) => {
 			headers.set("x-rapidapi-key", rapidApiKey);
-			headers.set("x-rapidapi-host", rapidApiHost);
+			headers.set(
+				"x-rapidapi-host",
+				"article-extractor-and-summarizer.p.rapidapi.com",
+			);
 			return headers;
 		},
 	}),
